5f839535825f9d14b40bb4fdcf61cc745f570253
BuildTools <christensentyler@ymail.com>
2018-10-07 14:39:41 -0500

    Oops, forgot to include commands in commandexecutor enum

--- src/main/java/proj/bot/ticket/command/CommandExecutor.java
+++ src/main/java/proj/bot/ticket/command/CommandExecutor.java
@@ -5,17 +5,28 @@ import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.User;
 import proj.bot.ticket.authenticator.Authenticator;
+import proj.bot.ticket.command.commands.Add;
+import proj.bot.ticket.command.commands.Blacklist;
+import proj.bot.ticket.command.commands.Close;
+import proj.bot.ticket.command.commands.Disable;
+import proj.bot.ticket.command.commands.Enable;
 import proj.bot.ticket.command.commands.Help;
 import proj.bot.ticket.command.commands.NA;
 import proj.bot.ticket.command.commands.NoPermission;
+import proj.bot.ticket.command.commands.Remove;
 import proj.bot.ticket.command.commands.TicketCreator;
 import proj.bot.ticket.supports.SupportType;
 
 public enum CommandExecutor {
+    ADD("ADD", new Add()),
+    BLACKLIST("BLACKLIST", new Blacklist()),
+    CLOSE("CLOSE", new Close()),
+    DISABLE("DISABLE", new Disable()),
+    ENABLE("ENABLE", new Enable()),
+    HELP("HELP", new Help()),
     NA("NA", new NA()),
     NP("NP", new NoPermission()),
-    HELP("HELP", new Help()),
-    
+    REMOVE("REMOVE", new Remove()),
     TICKET_CREATOR("TICKET_CREATOR", new TicketCreator()),
     ;
     
